c6154806c6697abe6cd1e59bd72cebb342863403
Luiz Adamchuk <adis_jr@hotmail.com>
2020-04-10 14:08:06 -0300

    Chamada a api e roda add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("repositories")
+      .then((response) => {
+        setRepositories(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  async function handleAddRepository() {}
 
   async function handleRemoveRepository(id) {
     // TODO
@@ -16,10 +29,7 @@ function App() {
       <ul data-testid="repository-list">
         <li>
           Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+          <button onClick={() => handleRemoveRepository(1)}>Remover</button>
         </li>
       </ul>
 
